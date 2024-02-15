2aadf489e18d7ac526b342207c82f7cfad3a7f41
MUKILAN019 <mukilan.p@kalvium.community>
2024-02-15 16:55:51 +0530

    added files

---
+++ Props/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Datacomp from './Components/Datacomponents';
+// import AppClass from "./AppClass";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    {/* <App />    */}
+    <Datacomp/>
+  </>
+);
